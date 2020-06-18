9e8a3e5d9115cf263130c72d63a4083896cd2380
shubh066 <54442585+shubh066@users.noreply.github.com>
2020-06-18 12:14:20 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,19 @@
+public class Array
+{
+
+public static void main(String args[])
+{
+  int a[][] = {{1,3,4}},{2,4,3},{3,4,5}};
+  int b[][] = {{1,3,4}},{2,4,3},{1,2,4}};
+  for(int i=0;i<3;i++)
+  {
+  for(int j=0;j<3;j++)
+  {
+    System.out.println(a[i][j]);
+    System.out.println(b[i][j]);
+  }
+  Systen.out.println();
+  }
+  }
+  }
+  
